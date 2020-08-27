d3d1d0691bf8ddc8fbdee2d33afad666678431a1
Dassaev <dassaev.tagle@gmail.com>
2020-08-27 09:38:34 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
-import 'zone.js';
+
 
 if (environment.production) {
   enableProdMode();
